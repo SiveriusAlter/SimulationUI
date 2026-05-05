18d7fec6ab50511d4c7ef72759e2774a40cafcf4
Alexander Zhernakov <neongenesisdatabase@gmail.com>
2026-05-06 01:02:41 +0500

    Add simple controller and marking

---
+++ src/main/java/org/example/simulationui/SimulationController.java
@@ -0,0 +1,41 @@
+package org.example.simulationui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SimulationController {
+
+    @FXML
+    private Button pauseButton;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button stopButton;
+
+    @FXML
+    private TextField worldSize;
+
+    @FXML
+    void inputWorldSize(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPauseClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStartClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStopClick(ActionEvent event) {
+
+    }
+}
